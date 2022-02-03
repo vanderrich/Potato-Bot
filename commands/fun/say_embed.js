ff6269b84174a74964c583ab85753af2529b2cb1
vanderrich <hendycoc8888@gmail.com>
2022-02-03 10:46:41 +0700

    e

--- commands/fun/say_embed.js
+++ commands/fun/say_embed.js
@@ -1,9 +1,8 @@
-const Discord = require('discord.js')
 module.exports= {
   name: 'sayembed',
   description: 'say something embed',
   usage: '<title, description>',
-  execute(message, args){
+  execute(message, args, cmd, client, Discord) {
     if(args[0] == null){
       message.channel.send('you didnt input any arguments!')
       return;
@@ -12,10 +11,13 @@ module.exports= {
     var title = args.slice(0, args.search(","))
     var description = args.slice(args.search(",")+1)
     
+    console.log(title, description)
+
     const embed = new Discord.MessageEmbed()
       .setColor('#0099ff')
       .setTitle(title)
       .setDescription(description)
+    console.log(embed)
     message.channel.send(embed)
     message.delete()
   }
