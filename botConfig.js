5ed653b1027552136b667575295af26c4ead8b33
vanderrich <hendycoc8888@gmail.com>
2022-02-14 07:08:56 +0700

    updated gitignore

--- botConfig.js
+++ botConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "ODk0MDYwMjgzMzczNDQ5MzE3.YVkghg.yHPb-fKt3wt8W-qcwzHM3k6TBIo",
+    token: "ODk0MDYwMjgzMzczNDQ5MzE3.YVkghg.kxba51FY7PToE0PVIw9ViGB9HIQ",
     prefix: "potat",
     admins: ["709950767670493275", "879207914005479514", "829171854681964604"],
     debug: true,
