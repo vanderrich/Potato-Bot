f160d0287748fdefd55f34c5db98091f6491868f
vanderrich <hendycoc8888@gmail.com>
2022-03-13 06:49:09 +0700

    fixed bug... by fix i mean delete

--- commands/info/dev.js
+++ commands/info/dev.js
@@ -4,26 +4,26 @@ module.exports = {
   description: 'Complete a form made by the server admin',
   category: "Info",
   async execute(message, args, cmd, client, Discord) {
-    if (!client.form.has(message.guild.id)) {
-      if (message.channel.permissionsFor(message.author).has('ADMINISTRATOR')) {
-        message.channel.send("Form Creator")
-        await message.channel.send("Enter a title for the Form").then({
+    // if (!client.form.has(message.guild.id)) {
+    //   if (message.channel.permissionsFor(message.author).has('ADMINISTRATOR')) {
+    //     message.channel.send("Form Creator")
+    //     await message.channel.send("Enter a title for the Form").then({
 
-        })
-      }
-    }
-    const form = client.form.get(message.guild.id)
-    async function collector(type, time, max) {
-      returned = []
-      if (type == "reaction") {
+    //     })
+    //   }
+    // }
+    // const form = client.form.get(message.guild.id)
+    // async function collector(type, time, max) {
+    //   returned = []
+    //   if (type == "reaction") {
 
-      } else if (type == "message") {
+    //   } else if (type == "message") {
 
-      }
-      const filter = m => m.author.id === message.author.id;
-      const collector = channel.createMessageCollector({ filter, time: time }, max);
-      collector.on('collect', m => returned.push(m));
-      collector.on('end', collected => returned.push(collected.size));
-    }
+    //   }
+    //   const filter = m => m.author.id === message.author.id;
+    //   const collector = channel.createMessageCollector({ filter, time: time }, max);
+    //   collector.on('collect', m => returned.push(m));
+    //   collector.on('end', collected => returned.push(collected.size));
+    // }
   }
 }
