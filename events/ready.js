32d93171cd5a0419c601065148f2711dc105ee52
vanderrich <hendycoc8888@gmail.com>
2022-03-12 11:18:02 +0700

    fixed the activity thing

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,9 @@
-const random_status = require('./../config.json')
+const { random_status } = require('./../config.json')
 module.exports = {
   name: 'ready',
   execute(client) {
     console.log('Ready!')
     client.user.setUsername('Potato bot')
-    client.user.setActivity(random_status[Math.floor(Math.Random * random_status.length)]);
+    client.user.setActivity(random_status[Math.floor(Math.random() * (random_status.length - 1))]);
   }
 }
