8c4cad06e557a8f04886ede419fcd0b9e326d5dc
vanderrich <hendycoc8888@gmail.com>
2022-04-07 09:04:50 +0700

    e

--- commands/adminonly/uninstallservice.js
+++ commands/adminonly/uninstallservice.js
@@ -5,6 +5,8 @@ module.exports = {
     category: "BotAdminOnly",
     execute(message, args, cmd, client) {
         if (!admins.includes(message.author.id)) return;
+        message.channel.send('uninstalling the service...')
         client.svc.uninstall();
+        message.channel.send('service uninstalled')
     }
 }
