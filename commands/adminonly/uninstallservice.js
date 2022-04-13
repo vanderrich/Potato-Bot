ccc2a2939fbc6400587284c893ed509e4f227b37
vanderrich <hendycoc8888@gmail.com>
2022-04-14 06:03:22 +0700

    uninstall uninstall service

--- commands/adminonly/uninstallservice.js
+++ commands/adminonly/uninstallservice.js
@@ -1,12 +1,12 @@
-const { admins } = require("../../config.json")
-const Service = require('node-windows').Service;
-module.exports = {
-    name: 'uninstallservice',
-    category: "Bot Admin Only",
-    execute(message, args, cmd, client) {
-        if (!admins.includes(message.author.id)) return;
-        message.reply('uninstalling the service...')
-        client.svc.uninstall();
-        message.reply('service uninstalled')
-    }
-}
+// const { admins } = require("../../config.json")
+// const Service = require('node-windows').Service;
+// module.exports = {
+//     name: 'uninstallservice',
+//     category: "Bot Admin Only",
+//     execute(message, args, cmd, client) {
+//         if (!admins.includes(message.author.id)) return;
+//         message.reply('uninstalling the service...')
+//         client.svc.uninstall();
+//         message.reply('service uninstalled')
+//     }
+// }
