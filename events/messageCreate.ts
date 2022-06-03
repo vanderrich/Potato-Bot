d6191e13c9d1dfadb43c14e8416fa21e50ce7d1c
vanderrich <hendycoc8888@gmail.com>
2022-06-03 17:07:21 +0700

    fix the uhh spam bug

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -11,11 +11,11 @@ module.exports = {
             });
             await guildSettings.save();
         }
-        for (let i = 0; i < guildSettings.badWords.length; i++) {
+        for (let i = 0; i < guildSettings?.badWords.length; i++) {
             if (message.channel.type === 'DM') break
             let badword
             try {
-                badword = message.content.toLowerCase().includes(guildSettings.badWords[i])
+                badword = message.content.toLowerCase().includes(guildSettings?.badWords[i])
             } catch { }
             if (badword) {
                 const m = await message.reply('Message contains a word in bad words list')
@@ -26,9 +26,9 @@ module.exports = {
         }
 
         //check if message is in the autoPublishChannels array
-        if (guildSettings.autoPublishChannels.length > 0) {
-            console.log(guildSettings.autoPublishChannels)
-            if (guildSettings.autoPublishChannels.find((channel: string) => channel === message.channel.id))
+        if (guildSettings?.autoPublishChannels.length > 0) {
+            console.log(guildSettings?.autoPublishChannels)
+            if (guildSettings?.autoPublishChannels.find((channel: string) => channel === message.channel.id))
                 return message.crosspost()
         }
 
