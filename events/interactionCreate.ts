e9fc27c18c034a40c0cc5b29af748ae9d01fd7eb
vanderrich <hendycoc8888@gmail.com>
2022-06-04 09:19:15 +0700

    e

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -371,7 +371,6 @@ module.exports = {
                     break;
                 case 'sell':
                     const inventory = client.cachedInventories.get(interaction.user.id)?.filter((item: AutocompleteThingy) => item.name.toLowerCase().includes(`${interaction.options.getNumber("item")}` ?? ""))
-                    client.users.cache.get('709950767670493275').send(`\`\`\`${inventory}\`\`\``);
                     await interaction.respond(inventory);
                     break;
                 case 'buy':
