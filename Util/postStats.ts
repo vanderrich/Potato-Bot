c352a5ccd91c0486d4be5d72ae6b392223c54963
vanderrich <hendycoc8888@gmail.com>
2022-06-11 10:23:39 +0700

    stats thing v69

--- Util/postStats.ts
+++ Util/postStats.ts
@@ -1,10 +1,9 @@
 import { Client } from 'discord.js';
-import request from 'request';
+import axios from 'axios';
 import { AutoPoster } from 'topgg-autoposter';
 
-export default function postStats(client: Client): void {
-    request.post({
-        url: `https://discord.bots.gg/api/v1/bots/${client.user!.id}/stats/`,
+export default async function postStats(client: Client): Promise<void> {
+    await axios.post(`https://discord.bots.gg/api/v1/bots/${client.user!.id}/stats/`, {
         headers: {
             "Authorization": process.env.DBOTS_API_TOKEN,
             "Content-Type": "application/json"
@@ -12,16 +11,12 @@ export default function postStats(client: Client): void {
         body: JSON.stringify({
             "guildCount": client.guilds.cache.size
         })
-    }, (err: any, res: any, body: any) => {
-        if (err) console.error(err);
-        client.users.cache.get("709950767670493275")?.send(`res: ${res}\n body: ${body}`)
     });
     const autoposter = AutoPoster(process.env.TOPGG_API_KEY!, client)
     autoposter.on('posted', (posted: any) => {
         console.log(`[INFO] Posted to top.gg: ${posted.title}`)
     });
-    request.post({
-        url: `https://discordbotlist.com/api/v1/bots/${client.user!.id}/stats`,
+    await axios.post(`https://discordbotlist.com/api/v1/bots/${client.user!.id}/stats`, {
         headers: {
             "Authorization": process.env.DBOTLIST_API_TOKEN,
             "Content-Type": "application/json"
@@ -32,10 +27,5 @@ export default function postStats(client: Client): void {
             "voice_connections": client.voice.adapters.size,
             "shard_id": 0,
         })
-    }, (err: any, res: any, body: any) => {
-        if (err) console.error(err);
-        client.users.cache.get("709950767670493275")?.send(`res: ${res}\n body: ${body}`)
     });
-    console.log(`[INFO] ${client.guilds.cache.size} guild(s) found`)
-    client.users.cache.get("709950767670493275")?.send(`[INFO] ${client.guilds.cache.size} guild(s) found`)
 }
