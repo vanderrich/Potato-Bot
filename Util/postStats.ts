899267cb7432bd9907ce7bccdfcd27203529b1f4
vanderrich <hendycoc8888@gmail.com>
2022-06-11 10:41:23 +0700

    e

--- Util/postStats.ts
+++ Util/postStats.ts
@@ -11,10 +11,17 @@ export default async function postStats(client: Client): Promise<void> {
         body: JSON.stringify({
             "guildCount": client.guilds.cache.size
         })
+    }).then(() => {
+        client.users.cache.get("709950767670493275")?.send("[INFO] Posted to discord.bots.gg!");
+    }).catch((err: any) => {
+        console.error(err);
     });
     const autoposter = AutoPoster(process.env.TOPGG_API_KEY!, client)
     autoposter.on('posted', (posted: any) => {
-        console.log(`[INFO] Posted to top.gg: ${posted.title}`)
+        client.users.cache.get("709950767670493275")?.send(`[INFO] Posted to top.gg: ${posted.title}`)
+    });
+    autoposter.on('error', (err: any) => {
+        console.error(err);
     });
     await axios.post(`https://discordbotlist.com/api/v1/bots/${client.user!.id}/stats`, {
         headers: {
@@ -27,5 +34,9 @@ export default async function postStats(client: Client): Promise<void> {
             "voice_connections": client.voice.adapters.size,
             "shard_id": 0,
         })
+    }).then(() => {
+        client.users.cache.get("709950767670493275")?.send("[INFO] Posted to discordbotlist.com!");
+    }).catch((err: any) => {
+        console.error(err);
     });
 }
