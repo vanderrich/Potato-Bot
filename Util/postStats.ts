e420918d25c643a14601d6e9af4eaf0ac3d9b33f
vanderrich <hendycoc8888@gmail.com>
2022-06-11 10:56:39 +0700

    e

--- Util/postStats.ts
+++ Util/postStats.ts
@@ -12,15 +12,9 @@ export default async function postStats(client: Client): Promise<void> {
         },
     });
     const autoposter = AutoPoster(process.env.TOPGG_API_KEY!, client)
-    autoposter.on('posted', (posted: any) => {
-        client.users.cache.get("709950767670493275")?.send(`[INFO] Posted to top.gg: ${posted.title}`)
-    });
-    autoposter.on('error', (err: any) => {
-        console.error(err);
-    });
     await axios.post(`https://discordbotlist.com/api/v1/bots/${client.user!.id}/stats`, {
         "guilds": client.guilds.cache.size,
-        "users": client.users.cache.size,
+        "users": undefined,
         "voice_connections": client.voice.adapters.size,
         "shard_id": 0,
     }, {
