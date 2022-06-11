5c716fbf2dd5974ce2ecc90e546d689b29d9d677
vanderrich <hendycoc8888@gmail.com>
2022-06-11 10:49:41 +0700

    help pls

--- Util/postStats.ts
+++ Util/postStats.ts
@@ -4,13 +4,12 @@ import { AutoPoster } from 'topgg-autoposter';
 
 export default async function postStats(client: Client): Promise<void> {
     await axios.post(`https://discord.bots.gg/api/v1/bots/${client.user!.id}/stats/`, {
+        "guildCount": client.guilds.cache.size
+    }, {
         headers: {
-            "Authorization": process.env.DBOTS_API_TOKEN,
+            "Authorization": process.env.DBOTS_API_TOKEN!,
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({
-            "guildCount": client.guilds.cache.size
-        })
     }).then(() => {
         client.users.cache.get("709950767670493275")?.send("[INFO] Posted to discord.bots.gg!");
     }).catch((err: any) => {
@@ -24,16 +23,15 @@ export default async function postStats(client: Client): Promise<void> {
         console.error(err);
     });
     await axios.post(`https://discordbotlist.com/api/v1/bots/${client.user!.id}/stats`, {
+        "guilds": client.guilds.cache.size,
+        "users": client.users.cache.size,
+        "voice_connections": client.voice.adapters.size,
+        "shard_id": 0,
+    }, {
         headers: {
-            "Authorization": process.env.DBOTLIST_API_TOKEN,
+            "Authorization": process.env.DBOTLIST_API_TOKEN!,
             "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "guilds": client.guilds.cache.size,
-            "users": client.users.cache.size,
-            "voice_connections": client.voice.adapters.size,
-            "shard_id": 0,
-        })
+        }
     }).then(() => {
         client.users.cache.get("709950767670493275")?.send("[INFO] Posted to discordbotlist.com!");
     }).catch((err: any) => {
