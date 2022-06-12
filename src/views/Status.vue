d5a9044b0084998973d17982bfe749adac0c7136
vanderrich <hendycoc8888@gmail.com>
2022-06-12 18:57:17 +0700

    e

--- src/views/Status.vue
+++ src/views/Status.vue
@@ -1,17 +1,3 @@
-<script lang="ts">
-import Vue from 'vue'
-import axios from 'axios'
-export default Vue.extend({
-    //listen to post requests
-    created() {
-        axios.post('/api/status')
-            .then(response => {
-                this.status = response.data
-            })
-    },
-})
-</script>
-
 <template>
     <div id="status">
         <h1>Status</h1>
