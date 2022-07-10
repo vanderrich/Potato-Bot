926fdc1eef339e5b647bc459f0f8e3277167afe3
vanderrich <hendycoc8888@gmail.com>
2022-07-10 10:44:29 +0700

    dictionary froth thojngsoymhuiyschvayen

--- slashCommands/Info/dictionary.ts
+++ slashCommands/Info/dictionary.ts
@@ -12,12 +12,12 @@ module.exports = {
                 .setName("word")
                 .setDescription("The word to look up")
                 .setRequired(true)
-    ) as SlashCommandBuilder,
+        ) as SlashCommandBuilder,
     category: "Info",
     async execute(interaction: CommandInteraction, client: Client, footers: string[]) {
         const word = interaction.options.getString("word");
-        const response = await axios.get(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.MERRIAMWEBSTER_API_KEY}`);
-        const data = response.data[0];
+        const response = await fetch(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.MERRIAMWEBSTER_API_KEY}`);
+        const data = (await response.json())[0];
         const locale = client.getLocale(interaction, "commands.info.dictionary");
 
         if (!data?.meta || !word) return interaction.reply({ content: client.getLocale(interaction, "commands.info.dictionary.noWord"), ephemeral: true });
