ba852c738c453d89cc5c40feeb7e1fd1efedae02
vanderrich <67942051+vanderrich@users.noreply.github.com>
2022-08-15 17:10:14 +0700

    stuff

--- slashCommands/Info/listbday.ts
+++ slashCommands/Info/listbday.ts
@@ -24,7 +24,7 @@ module.exports = {
         }))
         const embed = new MessageEmbed()
             .setColor('RANDOM')
-            .setAuthor({ name: client.getLocale(interaction, "commands.birthday.listEmbedTitle", interaction.guild ? interaction.guild.name : "all servers") })
+            .setAuthor({ name: client.getLocale(interaction, "commands.info.birthday.listEmbedTitle", interaction.guild ? interaction.guild.name : "all servers") })
             .setFooter({ text: footers[Math.floor(Math.random() * footers.length)] })
             .setTimestamp();
 
@@ -32,9 +32,11 @@ module.exports = {
             embed.setDescription(locale.noBdays);
             return interaction.editReply({ embeds: [embed] });
         } else {
+            console.log(birthdays);
             const pages: any[] = [];
             let page = 1, emptypage = false;
             do {
+                console.log(page);
                 const pageStart = 10 * (page - 1);
                 const pageEnd = pageStart + 10;
                 birthdays.filter((bday: Birthday) => client.guilds.cache.get(bday.guildId)?.members.cache.get(bday.userId))
@@ -42,27 +44,26 @@ module.exports = {
                     return `**${i + pageStart + 1}**. ${userMention(m.userId)} - ${time(m.birthday, 'd')}`;
                 });
                 if (items.length) {
-                    console.log(items);
                     embed.setDescription(`${items.join('\n')}${birthdays.length > pageEnd ? `\n... ${birthdays.length - pageEnd} more item(s)` : ''}`);
                     if (page % 2 === 0) embed.setColor('RANDOM');
                     else embed.setColor('RANDOM');
                     page++;
+                    console.log(page);
                 }
                 else {
                     emptypage = true;
                     if (page === 1) {
-                        embed.setDescription(locale.noBdays);
+                        embed.setDescription(locale.noBday);
                         embed.setFooter({ text: footers[Math.floor(Math.random() * footers.length)] });
-                        console.log(embed)
                         return interaction.editReply({ embeds: [embed] });
                     }
                     if (page === 2) {
-                        console.log(embed)
+                        console.log(embed);
                         return interaction.editReply({ embeds: [pages[0]] });
                     }
                 }
             } while (!emptypage);
-
+            console.log(pages);
             generatePages(interaction, pages, client, { timeout: 40000, fromButton: false });
         }
 
